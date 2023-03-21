29b2c066347c1c5960f8c16d17c6e692bbd9ab84
Maruf ahmmed pilow <81496426+Maruf-ahmmed-pilow@users.noreply.github.com>
2023-03-21 20:39:33 +0600

    Add files via upload

---
+++ assending and desending.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 20:20:04 2023
+
+@author: pilow
+"""
+
+nums = [1,5,3,4,2,10,6,8,7,9]
+
+nums.sort()
+
+print("list in Descending Order", nums)
+
+
+nums.sort(reverse=True)
+print("List in Assending Order", nums)
