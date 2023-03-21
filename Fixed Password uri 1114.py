92bcbbbde957192047b213cec1ffec0c0f3d5cb8
Maruf ahmmed pilow <81496426+Maruf-ahmmed-pilow@users.noreply.github.com>
2023-03-21 21:53:23 +0600

    Add files via upload

---
+++ Fixed Password uri 1114.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 21 21:39:39 2023
+
+@author: pilow
+"""
+
+
+while True:
+    password = int(input())
+    if password == 2002:
+        print("Acesso Permitido")
+        break
+    else:
+        print('Senha Invalida')
