1961b3b2148b762320d42a43f56658a00d4453a2
Maruf ahmmed pilow <81496426+Maruf-ahmmed-pilow@users.noreply.github.com>
2026-04-28 17:52:00 +0530

    Add files via upload

---
+++ write a programe to swap two variables without temp variable.py
@@ -0,0 +1,5 @@
+a = int(input('Enter a value of first variable: '))
+b = int(input('Enter a value of second variable: ')) 
+print(f"Before swapping: a = {a}, b = {b}")
+a, b=b,a
+print(f"After swapping: a = {a}, b = {b}")
