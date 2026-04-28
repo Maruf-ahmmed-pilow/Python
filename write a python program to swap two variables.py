8575272d9d207a8661562f12313d8bdec4aa49b6
Maruf ahmmed pilow <81496426+Maruf-ahmmed-pilow@users.noreply.github.com>
2026-04-28 14:50:29 +0530

    Add files via upload

---
+++ write a python program to swap two variables.py
@@ -0,0 +1,12 @@
+a = input('Enter the value of first variable: ')
+b= input('Enter the value of second variable: ')
+
+#displaying the value before swapping
+print(f"Before swapping: a = {a}, b = {b}")
+
+#swapping the values
+temp = a
+a = b
+b = temp
+#displaying the value after swapping
+print(f"After swapping: a ={a}, b = {b}")
